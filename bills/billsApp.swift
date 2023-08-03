7079cbae52f408f2fe6948fd17975d93a8d21064
unaivan <unaivan@gmail.com>
2023-08-03 10:11:09 +0700

    Initial Commit

---
+++ bills/billsApp.swift
@@ -0,0 +1,17 @@
+//
+//  billsApp.swift
+//  bills
+//
+//  Created by una ivan on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct billsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
